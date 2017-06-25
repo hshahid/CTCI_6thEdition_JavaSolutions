1a2c651282845e8c3cd8443479b0decca38652d0
Hashma <shahi22h@mtholyoke.edu>
2017-06-25 15:06:36 -0700

    solution

--- Chapter1.java
+++ Chapter1.java
@@ -5,29 +5,53 @@
 
 //1.1
 //a. With Hashtable
-public static boolean isUnique(String s) {
-    Set<Character> set = new HashSet<Character>();
-    
-    for(int i = 0; i<s.length();i++) {
-      if(set.contains(s.charAt(i))) {
-        return false;
-      }
-      
-      set.add(s.charAt(i));
+    public static boolean isUnique(String s) {
+        Set<Character> set = new HashSet<Character>();
+
+        for(int i = 0; i<s.length();i++) {
+          if(set.contains(s.charAt(i))) {
+            return false;
+          }
+
+          set.add(s.charAt(i));
+        }
+
+        return true;
     }
-    
-    return true;
-}
 
 //b. with array
-public static boolean isUnique(String s1) {
-    
-    boolean[] bool = new boolean[128];
-    
-    for (int i=0; i<s1.length();i++) {
-        if(bool[s1.charAt(i)] == true) { return false; } else {
-            bool[s1.charAt(i)] = true;
+    public static boolean isUnique(String s1) {
+
+        boolean[] bool = new boolean[128];
+
+        for (int i=0; i<s1.length();i++) {
+            if(bool[s1.charAt(i)] == true) { return false; } else {
+                bool[s1.charAt(i)] = true;
+            }
         }
+            return true;
     }
+
+//1.2
+    public static boolean checkPerm(String s1, String s2) {
+        //length
+        //add to int array
+        //if element is smaller than 0, then return false
+        int[] intArray = new int[128];
+        
+        if(s1.length() != s2.length()) { return false; }
+        
+        for (int i=0; i<s1.length(); i++) {
+            intArray[s1.charAt(i)] += 1;
+        }
+        
+        for (int i=0; i<s2.length(); i++) {
+                intArray[s2.charAt(i)] =  intArray[s2.charAt(i)] - 1;
+            if (intArray[s2.charAt(i)] < 0) {
+                return false;
+            }
+                                  
+        }
+        
         return true;
-}
+    }
--- Solution.java
+++ Solution.java
@@ -7,20 +7,33 @@
 
 class Solution {
     
-    public static boolean isUnique(String s1) {
-    
-    boolean[] bool = new boolean[128];
-    
-    for (int i=0; i<s1.length();i++) {
-        if(bool[s1.charAt(i)] == true) { return false; } else {
-            bool[s1.charAt(i)] = true;
+    public static boolean checkPerm(String s1, String s2) {
+        //length
+        //add to int array
+        //if element is smaller than 0, then return false
+        int[] intArray = new int[128];
+        
+        if(s1.length() != s2.length()) { return false; }
+        
+        for (int i=0; i<s1.length(); i++) {
+            intArray[s1.charAt(i)] += 1;
         }
-    }
+        
+        for (int i=0; i<s2.length(); i++) {
+                intArray[s2.charAt(i)] =  intArray[s2.charAt(i)] - 1;
+            if (intArray[s2.charAt(i)] < 0) {
+                return false;
+            }
+                                  
+        }
+        
         return true;
     }
+        
     
   public static void main(String[] args) {
-      String input = "hashma";
-      System.out.println(isUnique(input));
+      String s1 = "ratt";
+      String s2 = "ttar";
+      System.out.println(checkPerm(s1, s2));
   }
 }
